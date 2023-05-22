5ab454fc1b66b400bfca9d0a546c4938efffc01b
InsonusK <insonus.k@gmail.com>
2023-05-22 15:51:42 +0000

    move script 2

---
+++ scripts/Migrations/RemoveMigration.sh
@@ -0,0 +1,3 @@
+project='../../src/Configuration/Database/Instrument.Quote.Source.Configuration.DataBase/Instrument.Quote.Source.Configuration.DataBase.csproj'
+mig_pr='../../src/Configuration/Database/Instrument.Quote.Source.Configuration.DataBase.Migrations.Factory/Instrument.Quote.Source.Configuration.DataBase.Migrations.Factory.csproj'
+dotnet ef migrations remove -p $project -s $mig_pr
