c0f86ac704a2b2fb899a8cb4d81f79d2ebaa44de
InsonusK <insonus.k@gmail.com>
2023-05-22 15:50:33 +0000

    move script

--- scripts/AddMigration.sh
+++
@@ -1,4 +0,0 @@
-read -p 'Enter Migration name: ' migname
-project='../src/Configuration/Database/Instrument.Quote.Source.Configuration.DataBase/Instrument.Quote.Source.Configuration.DataBase.csproj'
-mig_pr='../src/Configuration/Database/Instrument.Quote.Source.Configuration.DataBase.Migrations.Factory/Instrument.Quote.Source.Configuration.DataBase.Migrations.Factory.csproj'
-dotnet ef migrations add $migname -p $project -s $mig_pr
