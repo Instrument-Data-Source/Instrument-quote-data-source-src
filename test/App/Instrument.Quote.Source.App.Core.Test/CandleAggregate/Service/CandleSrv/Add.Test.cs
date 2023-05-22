1d0a26341fe2e5c8b06a71bf67f8075767a70611
InsonusK <insonus.k@gmail.com>
2023-05-22 20:04:33 +0000

    remove test with db

--- test/App/Instrument.Quote.Source.App.Core.Test/CandleAggregate/Service/CandleSrv/Add.Test.cs
+++ test/App/Instrument.Quote.Source.App.Core.Test/CandleAggregate/Service/CandleSrv/Add.Test.cs
@@ -1,9 +1,15 @@
 using System.Net;
+using Instrument.Quote.Source.App.Core.CandleAggregate.Dto;
 using Instrument.Quote.Source.App.Core.CandleAggregate.Interface;
 using Instrument.Quote.Source.App.Core.CandleAggregate.Model;
 using Instrument.Quote.Source.App.Core.CandleAggregate.Service;
+using Instrument.Quote.Source.App.Core.CandleAggregate.Tool;
+using Instrument.Quote.Source.App.Core.Test.CandleAggregate.TestData;
+using Instrument.Quote.Source.App.Core.TimeFrameAggregate.Model;
 using Instrument.Quote.Source.Shared.Kernal.DataBase.Repository.Interface;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using MockQueryable.Moq;
 using NSubstitute;
 using Xunit.Abstractions;
 
@@ -20,12 +26,29 @@ public class Add_Test
   }
 
   [Fact]
-  public void WHEN_add_for_empty_instrument_THEN_Ok()
+  public async Task WHEN_add_for_empty_instrument_THEN_OkAsync()
   {
     // Array
+    var instument1 = new ent.Instrument("Inst1", "I1", 2, 3, new ent.InstrumentType(1));
+    instrumentRep.Table.Returns(new[] { instument1 }.BuildMock());
+    var expectedCandleDto = CandleFactory.RandomCandles(3, new DateTime(2020, 1, 1).ToUniversalTime()).Select(e => e.ToDto());
+    var expectedFrom = new DateTime(2020, 1, 1).ToUniversalTime();
+    var expectedUntill = new DateTime(2020, 1, 10).ToUniversalTime();
+
+    loadedPeriodRep.Table.Returns(new List<LoadedPeriod>().BuildMock());
 
     // Act
-    //srv.Add(1, 1, new DateTime(2020, 1, 1).ToUniversalTime(), new DateTime(2020, 1, 1).ToUniversalTime())
-    // Assert 
+    await srv.AddAsync(0, (int)TimeFrame.Enum.D1, expectedFrom, expectedUntill, expectedCandleDto);
+
+    // Assert
+    //loadedPeriodRep.Received().AddAsync(Arg.Any<LoadedPeriod>(), Arg.Any<IDbContextTransaction>(), Arg.Any<CancellationToken>());
+    loadedPeriodRep.Received().AddAsync(Arg.Is<LoadedPeriod>(e =>
+        e.FromDate == expectedFrom &&
+        e.UntillDate == expectedUntill &&
+        e.InstrumentId == 0 &&
+        e.TimeFrameId == (int)TimeFrame.Enum.D1 &&
+        e.Candles.Count() == expectedCandleDto.Count()),
+    Arg.Any<IDbContextTransaction>(),
+    Arg.Any<CancellationToken>());
   }
 }
