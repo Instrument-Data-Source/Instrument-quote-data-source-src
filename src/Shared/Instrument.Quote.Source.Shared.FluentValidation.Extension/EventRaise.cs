e358547a436c2819833fe8de8d86bb67be10e6bc
InsonusK <insonus.k@gmail.com>
2023-05-25 00:07:54 +0000

    #29 Add validation extension

---
+++ src/Shared/Instrument.Quote.Source.Shared.FluentValidation.Extension/EventRaise.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+using FluentValidation;
+namespace Instrument.Quote.Source.Shared.FluentValidation.Extension;
+public static class EventRaise
+{
+  public static void WithEventId<T, TProperty>(this IRuleBuilderOptions<T, TProperty> ruleBuilder, EventId eventId)
+  {
+    ruleBuilder.WithErrorCode(eventId.Id.ToString()).WithMessage(eventId.Name);
+  }
+}
